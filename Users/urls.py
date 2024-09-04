8b4d0fe37daaec3dc136a9d3188d74321307478a
SantiagoSzpakowski <szsantiago97@gmail.com>
2024-09-03 21:12:52 -0300

    Login y logout

--- Users/urls.py
+++ Users/urls.py
@@ -1,8 +1,10 @@
 from Users import views
 from django.urls import path
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
-    path('usuarios/', views.usuarios, name='usuarios'),
+    path('usuarios/', views.loginUsuario, name='usuarios'),
     path('registro/', views.registroUsuario, name='registro'),
+    path('logout/', LogoutView.as_view(next_page='inicio'), name="logout"),
 
 ]
--- Users/views.py
+++ Users/views.py
@@ -6,14 +6,10 @@ from Users.forms import UserRegisterForm
 # Create your views here.
 
 
-def usuarios(req):
-    return render(req,'users/usuarios.html')
-
 def registroUsuario(req):
     msg_register = ""
     if req.method == 'POST':
         form = UserRegisterForm(req.POST)
-        print(f"Request method: {form}")
         if form.is_valid():
             user = form.save()
             login(req, user)
@@ -23,46 +19,28 @@ def registroUsuario(req):
             msg_register += f" | {form.errors}"
     
     form = UserRegisterForm()
-    return render(req,"users/registro.html" ,  {"form":form})
+    return render(req,"users/registro.html" ,  {"form":form, "msg_register": msg_register})
 
 
 
 # Create your views here.
-def login_request(request):
+def loginUsuario(req):
     msg_login = ""
-    if request.method == 'POST':
-        form = AuthenticationForm(request, data=request.POST)
-
+    if req.method == 'POST':
+        form = AuthenticationForm(req, data=req.POST)
+        
         if form.is_valid():
-
             usuario = form.cleaned_data.get('username')
-            contrasenia = form.cleaned_data.get('password')
-
-            user = authenticate(username= usuario, password=contrasenia)
+            clave = form.cleaned_data.get('password')
+            user = authenticate(username= usuario, password=clave)
 
             if user is not None:
-                login(request, user)
-                return render(request, "AppCoder/index.html")
+                login(req, user)
+                return redirect("inicio")
 
         msg_login = "Usuario o contraseña incorrectos"
 
     form = AuthenticationForm()
-    return render(request, "users/login.html", {"form": form, "msg_login": msg_login})
+    return render(req, "users/usuarios.html", {"form": form, "msg_login": msg_login})
 
-# Vista de registro
-#def register(request):
-    msg_register = ""
-    if request.method == 'POST':
 
-        form = UserRegisterForm(request.POST)
-        if form.is_valid():
-            # Si los datos ingresados en el form son válidos, con form.save()
-            # creamos un nuevo user usando esos datos
-            form.save()
-            return render(request,"AppCoder/index.html")
-        else:
-            msg_register = "Error en los datos ingresados"
-            msg_register += f" | {form.errors}"
-
-    form = UserRegisterForm()     
-    return render(request,"users/registro.html" ,  {"form":form, "msg_register": msg_register})
